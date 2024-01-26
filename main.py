72b8eeb1bf80963c74c712402e520f7ad89ac22c
MateuszKociolek <mkociolek2005@gmail.com>
2024-01-26 11:21:34 +0100

    Added username input and scoring points on server site

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO
-from tools.returnJsonFile import getQuestionsJson
+from tools.returnJsonFile import getQuestionsJson, printPoints
 import uuid
-
 
 app = Flask(__name__)
 socketio = SocketIO(app)
+
+playersPoints = {}
 
 @app.route('/')
 def index():
@@ -15,6 +16,12 @@ def index():
 def renderPhone():
     userId = str(uuid.uuid4())
     print(f"User (id: {userId}) has joined!")
+    newRecord = {
+        "username" : "testTest",
+        "points" : 0
+    }
+    playersPoints[userId] = newRecord
+
     return render_template('phoneView.html', id=userId)
 
 @app.route('/pcView')
@@ -23,15 +30,27 @@ def renderPc():
 
 @socketio.on("startQuiz")
 def sendQuestion():
+    playersPoints = {}
     socketio.emit("sendQuestions", getQuestionsJson())
+
+@socketio.on("setUsername")
+def setUsername(data):
+    playersPoints[data["id"]]["username"] = data["username"]
+    printPoints(playersPoints)
+
 
 @socketio.on('answer')
 def getAnswer(data):
     print(f"User {data['userId']} answered: {data['answer']}")
+    if data['answer']:
+        if data['userId'] in playersPoints:
+            playersPoints[data['userId']]["points"] += 100
+
 
 @socketio.on('nextQuestion')
 def nextQuestion(data):
     socketio.emit('nextQuestion', {"questionId" : data["id"], "base": getQuestionsJson()})
+    printPoints(playersPoints)
 
 if __name__ == "__main__":
     socketio.run(app,host="0.0.0.0", port=5000 , debug=True)
--- tools/returnJsonFile.py
+++ tools/returnJsonFile.py
@@ -4,3 +4,7 @@ def getQuestionsJson():
     with open("questions.json", "r" ,encoding='utf-8') as js:
         jsonToReturn = json.load(js)
     return jsonToReturn
+
+def printPoints(pointsList):
+    for i in pointsList:
+        print(pointsList[i]["username"], pointsList[i]["points"])
